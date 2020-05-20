f7f85c1413dc69ae9d85b651838baf2a24cf9051
Brandon Bashein <brandon@Brandons-MacBook-Pro.local>
2020-05-20 10:23:28 -0600

    created basic view

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+interface ITodoItem {
+  text: string
+  complete: Number
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +12,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'client';
+  public text = ''
+  // sqlite does not have type bool just 1 and 0
+  public complete = 0
+  public todos: ITodoItem[] = []
+
+  constructor(private httpClient: HttpClient) { }
+
+  async ngOnInit() {
+    const loaded = await this.loadTodos()
+    console.log(loaded)
+  }
+
+  async loadTodos() {
+    this.todos = await this.httpClient
+      .get<ITodoItem[]>('/api/todo')
+      .toPromise()
+  }
+
+  async addTodo() {
+    this.httpClient.post('/api/todo', {
+      title: this.text,
+      complete: this.complete
+    })
+    await this.loadTodos()
+    this.text = ''
+    this.complete = 0
+  }
 }
